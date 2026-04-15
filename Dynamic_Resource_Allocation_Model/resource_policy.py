460f268886d2b3e8cf4510b24dd7f28976ae35db
Kimhj123 <89951779+Kimhj123@users.noreply.github.com>
2026-04-15 21:27:05 +0900

    Update resource_policy.py

--- Dynamic_Resource_Allocation_Model/resource_policy.py
+++ Dynamic_Resource_Allocation_Model/resource_policy.py
@@ -1,14 +1,12 @@
-def allocate_resource(pred_rps, current_rps=None):
-    safe_rps = pred_rps * 1.15
-    
-    if current_rps is not None and (pred_rps - current_rps) > 50:
-        safe_rps = max(safe_rps, 450)
-        
-    if safe_rps < 150:
+def allocate_resource(pred_rps):
+    """
+    예측된 RPS를 기반으로 (CPU, Replicas)를 결정하는 정책 함수.
+    """
+    if pred_rps < 150:
         return 0.5, 1
-    elif safe_rps < 280:
+    elif pred_rps < 250:
         return 1.0, 2
-    elif safe_rps < 450:
-        return 2.0, 4
+    elif pred_rps < 400:
+        return 2.0, 3
     else:
-        return 4.0, 6
+        return 3.0, 5
